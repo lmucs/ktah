56765fe0c41721d89b21760271a9021f669a9f75
Andrew Forney <forneyandrew@gmail.com>
2011-09-15 00:53:58 -0700

    -changed names on some classes to reflect recent gameplay developments (they are, however, still stubs)

---
+++ src/main/js/types/characters/chemist.js
@@ -0,0 +1,11 @@
+/**
+ *
+ * chemist.js
+ *
+ * constructor for a chemist character
+ */
+
+function chemist(name, level) {
+  this.name = name;
+  this.level = level;
+}
---
+++ src/main/js/types/characters/pioneer.js
@@ -0,0 +1,12 @@
+/**
+ *
+ * pioneer.js
+ *
+ * constructor for a pioneer character
+ */
+
+function pioneer(name, level)
+{
+  this.name = name;
+  this.level = level;
+}
