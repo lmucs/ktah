9d75287eac2559e930b0192a359bd35b3caf2869
Forns <forneyandrew@gmail.com>
2011-09-15 17:15:20 -0700

    -changed ktah.js to setup.js and cleaned up the global object creation (much stolen from M3... shamelessly)

---
+++ src/main/js/setup.js
@@ -0,0 +1,17 @@
+/**
+ * setup.js
+ * 
+ * Contains the single global object, ktah, which contains
+ * all other game components.
+ * 
+ * !!! Include this script before all others !!!
+ *
+ */
+
+// Global ktah object
+var ktah = {
+    
+    // Container object to hold the types
+    types: {}
+    
+};
