fc3db512138b7c6f7a7679062b4465d6880b92b8
Forns <forneyandrew@gmail.com>
2011-09-15 18:20:18 -0700

    -added stub for the herder class

---
+++ src/main/js/types/characters/herder.js
@@ -0,0 +1,27 @@
+/**
+ * herder.js
+ *
+ * The type of "herder" containing class attributes.
+ *
+ * Acting in an atypically synergistic fashion with the 
+ * horde, the herder serves to mislead and manipulate 
+ * zombies so that other team mates have time to gather 
+ * resources and perform abilities.
+ *
+ */
+
+$(function () {
+    ktah.types.Architect = function () {
+        return{
+            // Public properties
+            zombieFlesh: 0,
+            atomicWaste: 0,
+            
+            // "Constructor"
+            create: function () {
+                
+            }
+        
+        }
+    }
+});
