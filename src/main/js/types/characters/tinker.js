63fcee2d3e0cb9bb0b4a7ed7d7b295147a344216
Forns <forneyandrew@gmail.com>
2011-09-15 18:15:49 -0700

    -added tinker class with stub properties

---
+++ src/main/js/types/characters/tinker.js
@@ -0,0 +1,29 @@
+/**
+ * tinker.js
+ *
+ * The type of "tinker" containing class attributes.
+ *
+ * The tinker "excels" in a variety of abilities that may greatly
+ * aid or hinder his team and their abilities. Aside from most of 
+ * his abilities relying on a toss of the dice, he is the only class
+ * capable of consistently chipping away at members of the horde,
+ * able to disable the occasional member for an entire round.
+ *
+ */
+
+$(function () {
+    ktah.types.Tinker = function () {
+        return{
+            // Public properties
+            stone: 0,
+            wood: 0,
+            atomicWaste: 0,
+            
+            // "Constructor"
+            create: function () {
+                
+            }
+        
+        }
+    }
+});
