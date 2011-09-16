7de9dea56956ca666813ab5d99c9f605773b43ce
Forns <forneyandrew@gmail.com>
2011-09-15 18:05:12 -0700

    -added "constructor" and general qualities for characters

--- src/main/js/types/characters/character.js
+++ src/main/js/types/characters/character.js
@@ -1,33 +1,26 @@
 /**
- *
  * charClass.js
  *
- * prototype constructor for ktah character classes
- *
- * dependencies: classes/teacher.js, 
+ * Contains general character attributes between classes.
+ * 
  */
 
-//Class id's:
-var TEACHER = 0;
-var TINKERER = 1;
-var SCIENTIST = 2;
-
-function charClass(class, name, level)
-{
-  this.class = class;
-  
-  switch (this.class)
-  {
-    case TEACHER:
-      return new teacher(name, level);
-      break;
-    case TINKERER:
-      return new tinkerer(name, level);
-      break;
-    case SCIENTIST:
-      return new scientist(name, level);
-      break;
-    default:
-      break;
-  }
-}
+$(function () {
+    ktah.types.Character = function () {
+        return {
+            // Public properties
+            name: "",
+            level: 0,
+            health: 10, // TODO: Arbitrary placeholder
+            characterClass: "",
+            
+            // "Constructor"
+            create: function (name, level, health, charClass) {
+                this.name = name;
+                this.level = level;
+                this.health = health;
+                this.characterClass = charClass;
+            }
+        };
+    };
+});
