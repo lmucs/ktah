1122b12b9515c5eb9af9482f0cb672e04a9eac18
Forns <forneyandrew@gmail.com>
2011-09-20 00:46:45 -0700

    - Added copperlicht-ui.js to modules; will continue to flesh out

---
+++ src/main/public/js/modules/copperlicht-ui.js
@@ -0,0 +1,13 @@
+/**
+ * copperlicht-ui.js
+ *
+ * Contains the basic user-interaction elements
+ * that copperlicht uses to update its display.
+ */
+
+$(function () {
+    // Begin by binding the keypress event to the canvas object
+    $("body").keypress(function (event) {
+        
+    });
+});
