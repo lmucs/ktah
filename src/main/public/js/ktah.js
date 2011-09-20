d407b93ec17ba2f56dedc0721646bc52dbedca35
David Hara <Hakuhen@gmail.com>
2011-09-19 22:32:26 -0700

    changed includes to $.getScripts to ensure ordered loading

--- src/main/public/js/ktah.js
+++ src/main/public/js/ktah.js
@@ -4,37 +4,35 @@
  * ALL GLORY TO KTAH!
  */
 
-// TODO: Do we want to include jQuery here, or keep it in our html file?
-
 var ktah = {
 
   types: {}
 
 };
 
-// TODO: Should all of these includes be in their own script (init.js), and then just that is included here?
+// TODO: Should all of these includess be in their own script (init.js), and then just that is included here?
 
 /***** LIBRARIES *****/
-include('../js/lib/copperlicht.js');
-include('../js/lib/base2.min.js');
+$.getScript('../js/lib/copperlicht.js');
+$.getScript('../js/lib/base2.min.js');
 
 /***** TYPES *****/
     
     // Character classes
-    include('../js/types/characters/character.js');
-    include('../js/types/characters/pioneer.js');
-    include('../js/types/characters/architect.js');
-    include('../js/types/characters/chemist.js');
-    include('../js/types/characters/herder.js');
-    include('../js/types/characters/land-master.js');
-    include('../js/types/characters/tinker.js');
+    $.getScript('../js/types/characters/character.js');
+    $.getScript('../js/types/characters/pioneer.js');
+    $.getScript('../js/types/characters/architect.js');
+    $.getScript('../js/types/characters/chemist.js');
+    $.getScript('../js/types/characters/herder.js');
+    $.getScript('../js/types/characters/land-master.js');
+    $.getScript('../js/types/characters/tinker.js');
     
     // Resources
-    include('../js/types/resources/resource.js');
-    include('../js/types/resources/stone.js');
-    include('../js/types/resources/atomic-waste.js');
-    include('../js/types/resources/metal.js');
-    include('../js/types/resources/sand.js');
-    include('../js/types/resources/water.js');
-    include('../js/types/resources/wood.js');
-    include('../js/types/resources/zombie-flesh.js');
+    $.getScript('../js/types/resources/resource.js');
+    $.getScript('../js/types/resources/stone.js');
+    $.getScript('../js/types/resources/atomic-waste.js');
+    $.getScript('../js/types/resources/metal.js');
+    $.getScript('../js/types/resources/sand.js');
+    $.getScript('../js/types/resources/water.js');
+    $.getScript('../js/types/resources/wood.js');
+    $.getScript('../js/types/resources/zombie-flesh.js');
