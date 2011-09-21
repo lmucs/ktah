df485b05bbb4a144733990958b34bef95f6b070c
Forns <forneyandrew@gmail.com>
2011-09-21 01:31:37 -0700

    - Fixed spacing and jslint happiness things (minor)

--- src/main/app.js
+++ src/main/app.js
@@ -1,52 +1,52 @@
-
 /**
+ * app.js
+ * 
  * Module dependencies.
  */
 
-var express = require('express');
-
-var app = module.exports = express.createServer();
+var express = require('express'),
+    app = module.exports = express.createServer();
 
 // Configuration
 
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.set("view options", {layout: false});
-  app.register('.html', {
-    compile: function(str, options){
-      return function(locals){
-        return str;
-      };
-    }
-  });
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(express.cookieParser());
-  app.use(express.session({ secret: 'your secret here' })); // TODO I don't know much about sessions and what this secret is, but we need to pick a secret!
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
+app.configure(function () {
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+    app.set("view options", {layout: false});
+    app.register('.html', {
+        compile: function(str, options){
+            return function(locals){
+                return str;
+            };
+        }
+    });
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(express.cookieParser());
+    app.use(express.session({ secret: 'your secret here' })); // TODO I don't know much about sessions and what this secret is, but we need to pick a secret!
+    app.use(app.router);
+    app.use(express.static(__dirname + '/public'));
 });
 
 app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
 });
 
 app.configure('production', function(){
-  app.use(express.errorHandler()); 
+    app.use(express.errorHandler()); 
 });
 
 // Routes
 
 app.get('/', function(req, res){
-  res.render('index', {
-  	layout: true,
-    pageTitle: "K'tah!"
-  });
+    res.render('index', {
+        layout: true,
+        pageTitle: "K'tah!"
+    });
 });
 
 app.get('/zombietest', function(req, res){
-  res.render('zombieTest.html');
+    res.render('zombieTest.html');
 });
 
 app.listen(3000);
