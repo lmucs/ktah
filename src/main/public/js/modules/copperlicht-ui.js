25b09d9e517a81dca7037cb95f29f9d6a66ea895
Forns <forneyandrew@gmail.com>
2011-09-21 01:32:08 -0700

    - Added copperlicht zombie demo functionality for the index, but will need to troubleshoot

--- src/main/public/js/modules/copperlicht-ui.js
+++ src/main/public/js/modules/copperlicht-ui.js
@@ -6,8 +6,43 @@
  */
 
 $(function () {
-    // Begin by binding the keypress event to the canvas object
-    $("body").keypress(function (event) {
         
+    // Begin by setting up the canvas element
+    var engine = startCopperLichtFromFile('game', '../../assets/copperlicht/copperlichtdata/zombieTest.ccbjs'),
+        zombieSceneNode = null,
+        scene = null,
+        key = null;
+    
+    // Called when loading the 3d scene has finished (from the coppercube file)
+    engine.OnLoadingComplete = function () {
+        scene = engine.getScene();
+        if (scene) {
+            // Grab the zombieSceneNode scene node
+            zombieSceneNode = scene.getSceneNodeFromName('ghoul');
+        }
+    }
+    
+    // Next, bind the keypress event to the canvas object
+    $("game").keypress(function (event) {
+        key = String.fromCharCode(event.keyCode);
+        
+        // When pressing w, move forward, s back
+        // a move left, d move right
+        switch (key) {
+            case 'w':
+                zombieSceneNode.Pos.X -= 1;
+                break;
+            case 's':
+                zombieSceneNode.Pos.X += 1;
+                break;
+            case 'a':
+                zombieSceneNode.Pos.Z -= 1;
+                break;
+            case 'd':
+                zombieSceneNode.Pos.Z += 1;
+                break;
+            default:
+                break;
+         }
     });
 });
