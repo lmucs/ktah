fdafc4f5b6a3f85c27f1abad5ae3d2d9a6b3a09f
Andrew Forney <forneyandrew@gmail.com>
2011-09-23 20:54:05 -0700

    - Restructured to support linear imports for dependencies

--- src/main/public/js/util/include.js
+++ src/main/public/js/util/include.js
@@ -1,11 +1,60 @@
 /**
  * include.js
  *
- * Utility script to include js files within other js files.
+ * Utility script to include js files within other js files,
+ * so you can js file while you js file.
  */
 
-include = function(src) {
-    var script = document.createElement("script");
-    script.setAttribute("src", src)
-    document.getElementsByTagName("head")[0].appendChild(script);
-}
+$(function () {
+
+    // Array containing all necessary imports
+    var directories = [
+    
+        /***** LIBRARIES *****/
+        '/js/lib/copperlicht.js',
+        '/js/lib/base2.min.js',
+        
+        /***** MODULES *****/
+        '/js/modules/copperlicht-ui.js',
+        
+        /***** TYPES *****/
+        // Character classes
+        '/js/types/characters/character.js',
+        '/js/types/characters/pioneer.js',
+        '/js/types/characters/architect.js',
+        '/js/types/characters/chemist.js',
+        '/js/types/characters/herder.js',
+        '/js/types/characters/land-master.js',
+        '/js/types/characters/tinker.js',
+        
+        // Resources
+        '/js/types/resources/resource.js',
+        '/js/types/resources/stone.js',
+        '/js/types/resources/atomic-waste.js',
+        '/js/types/resources/metal.js',
+        '/js/types/resources/sand.js',
+        '/js/types/resources/water.js',
+        '/js/types/resources/wood.js',
+        '/js/types/resources/zombie-flesh.js'
+    
+    ],
+    
+    // Number of imports needed to be handled, plus a counter, n
+    numberOfImports = directories.length,
+    n = 0;
+    
+    // Helper function to append the scripts (call once at start)
+    ktah.utils.include = function () {
+        // Only import when there are more to go
+        if (n < numberOfImports) {
+            var script = document.createElement("script");
+            script.setAttribute("src", directories[n]);
+            document.getElementsByTagName("head")[0].appendChild(script);
+            n++;
+        }
+    };
+    
+    // Call once to get the ball rolling
+    ktah.utils.include();
+    
+});
