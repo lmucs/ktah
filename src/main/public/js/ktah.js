102df8911bf1be492107fbc46069db17e9dc58a3
Andrew Forney <forneyandrew@gmail.com>
2011-09-23 20:53:47 -0700

    - Moved imports into include.js - Added utils property of ktah

--- src/main/public/js/ktah.js
+++ src/main/public/js/ktah.js
@@ -6,36 +6,7 @@
 
 var ktah = {
 
-  types: {}
+  types: {},
+  utils: {}
 
 };
-
-// TODO: Should all of these includes be in their own script (init.js), and then just that is included here?
-
-/***** LIBRARIES *****/
-$.getScript('../js/lib/copperlicht.js');
-$.getScript('../js/lib/base2.min.js');
-
-/***** MODULES *****/
-$.getScript('../js/modules/copperlicht-ui.js');
-
-/***** TYPES *****/
-    
-    // Character classes
-    $.getScript('../js/types/characters/character.js');
-    $.getScript('../js/types/characters/pioneer.js');
-    $.getScript('../js/types/characters/architect.js');
-    $.getScript('../js/types/characters/chemist.js');
-    $.getScript('../js/types/characters/herder.js');
-    $.getScript('../js/types/characters/land-master.js');
-    $.getScript('../js/types/characters/tinker.js');
-    
-    // Resources
-    $.getScript('../js/types/resources/resource.js');
-    $.getScript('../js/types/resources/stone.js');
-    $.getScript('../js/types/resources/atomic-waste.js');
-    $.getScript('../js/types/resources/metal.js');
-    $.getScript('../js/types/resources/sand.js');
-    $.getScript('../js/types/resources/water.js');
-    $.getScript('../js/types/resources/wood.js');
-    $.getScript('../js/types/resources/zombie-flesh.js');
