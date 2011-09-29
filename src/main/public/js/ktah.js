9d48c721d5149e79771449059e3831fd70a861fc
David Hara <hakuhen@gmail.com>
2011-09-29 11:38:26 -0700

    abstractified (YEAAAH!) include.js for generic usage.

--- src/main/public/js/ktah.js
+++ src/main/public/js/ktah.js
@@ -6,6 +6,36 @@
 
 var ktah = {
 
-  types: {}
+  types: {},
 
 };
+
+include.includeInit([
+  
+  /***** LIBRARIES *****/
+  '/js/lib/copperlicht.js',
+  '/js/lib/base2.min.js',
+  
+  /***** MODULES *****/
+  '/js/modules/copperlicht-ui.js',
+  
+  /***** TYPES *****/
+  // Character classes
+  '/js/types/characters/character.js',
+  '/js/types/characters/pioneer.js',
+  '/js/types/characters/architect.js',
+  '/js/types/characters/chemist.js',
+  '/js/types/characters/herder.js',
+  '/js/types/characters/land-master.js',
+  '/js/types/characters/tinker.js',
+  
+  // Resources
+  '/js/types/resources/resource.js',
+  '/js/types/resources/stone.js',
+  '/js/types/resources/atomic-waste.js',
+  '/js/types/resources/metal.js',
+  '/js/types/resources/sand.js',
+  '/js/types/resources/water.js',
+  '/js/types/resources/wood.js',
+  '/js/types/resources/zombie-flesh.js'
+]);
--- src/main/public/js/util/include.js
+++ src/main/public/js/util/include.js
@@ -5,57 +5,31 @@
  * so you can js file while you js file.
  */
 
-$(function () {
+var include = {
 
     // Array containing all necessary imports
-    var directories = [
-    
-        /***** LIBRARIES *****/
-        '/js/lib/copperlicht.js',
-        '/js/lib/base2.min.js',
-        
-        /***** MODULES *****/
-        '/js/modules/copperlicht-ui.js',
-        
-        /***** TYPES *****/
-        // Character classes
-        '/js/types/characters/character.js',
-        '/js/types/characters/pioneer.js',
-        '/js/types/characters/architect.js',
-        '/js/types/characters/chemist.js',
-        '/js/types/characters/herder.js',
-        '/js/types/characters/land-master.js',
-        '/js/types/characters/tinker.js',
-        
-        // Resources
-        '/js/types/resources/resource.js',
-        '/js/types/resources/stone.js',
-        '/js/types/resources/atomic-waste.js',
-        '/js/types/resources/metal.js',
-        '/js/types/resources/sand.js',
-        '/js/types/resources/water.js',
-        '/js/types/resources/wood.js',
-        '/js/types/resources/zombie-flesh.js'
-    
-    ],
+    directories: [],
     
     // Number of imports needed to be handled, plus a counter, n
-    numberOfImports = directories.length,
-    n = 0,
+    numberOfImports: 0,
+    n: 0,
     
     // Helper function to append the scripts (call once at start)
-    include = function () {
+    run: function () {
         // Only import when there are more to go
         if (n < numberOfImports) {
             var script = document.createElement("script");
             script.setAttribute("src", directories[n]);
-            script.onload = include;
+            script.onload = include.run;
             document.getElementsByTagName("head")[0].appendChild(script);
             n++;
         }
-    };
+    },
     
-    // Call once to get the ball rolling
-    include();
-    
-});
+    includeInit: function (d) {
+        directories = d;
+        numberOfImports = directories.length;
+        n = 0;
+        include.run();
+    }
+}
