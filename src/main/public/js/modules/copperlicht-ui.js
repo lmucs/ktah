59445ea2b67d5bd4318695f7281598f77afc6450
Brian Handy <BlinksTale@gmail.com>
2011-10-05 19:55:03 -0700

    Better controls, not perfect. Supports click vs. hold for mouse movement

--- src/main/public/js/modules/copperlicht-ui.js
+++ src/main/public/js/modules/copperlicht-ui.js
@@ -26,6 +26,8 @@ $(function() {
   // Mouse Controls values
   goalX = NaN, // where to travel to
   goalZ = NaN, // where to travel to
+  originalX = NaN,
+  originalZ = NaN,
   engWidth = 640, // width of clickable area
   engHeight = 480, // height of clickable area
   mouseToDist = 25, // conversion ratio for mouse units to ingame distance units
@@ -40,6 +42,9 @@ $(function() {
   zoomSpeed = -2,
   isometricView = true,
   cameraStarted = false,
+  mouseIsDown = false,
+  mouseClicked = false,
+  mouseClickedTimer = 0,
 
   // Variables for keyboard controls
   wKey = aKey = sKey = dKey = false, upKey = leftKey = downKey = rightKey = false;
@@ -63,7 +68,7 @@ $(function() {
     scene.getRootSceneNode().addChild(cam);
     scene.setActiveCamera(cam);
   }
-  // Default camera commands
+  // Default camera instructions
   camFollow = function(cam, target) {
     if (isometricView) {
       isometricCam(cam, target);
@@ -85,11 +90,19 @@ $(function() {
     cam.Pos.Z = target.Pos.Z - (camSetDist + zoomDist);
     animator.lookAt(new CL3D.Vect3d(zombieSceneNode.Pos.X, zombieSceneNode.Pos.Y + 10, zombieSceneNode.Pos.Z));
   }
-  // A more complicated keydown event. Uppercase and lowercase
-  // letters referenced due to keydown vs keypress differences
+  // Pass keydown to keyStateChange
   document.onkeydown = function(event) {
     key = String.fromCharCode(event.keyCode);
-
+    keyStateChange(key, true);
+  }
+  // Pass keyup to keyStateChange
+  document.onkeyup = function(event) {
+    key = String.fromCharCode(event.keyCode);
+    keyStateChange(key, false);
+  }
+  // A more complicated key change state event. Uppercase and lowercase
+  // letters both referenced due to keydown vs keypress differences
+  keyStateChange = function(key, bool) {
     // Displays key value, for learning new key cases
     // alert(key);
     // When pressing w, move forward, s back
@@ -99,68 +112,54 @@ $(function() {
       case 'W':
       case '&':
         // up arrow
-        wKey = true;
+        wKey = bool;
         break;
       case 's':
       case 'S':
       case '(':
         // down arrow
-        sKey = true;
+        sKey = bool;
         break;
       case 'a':
       case 'A':
       case '%':
         // left arrow
-        aKey = true;
+        aKey = bool;
         break;
       case 'd':
       case 'D':
       case "'":
         // right arrow
-        dKey = true;
+        dKey = bool;
         break;
       default:
         break;
     }
   }
-  // Set up simple example keydown event
-  document.onkeyup = function(event) {
-    key = String.fromCharCode(event.keyCode);
-
-    // When pressing w, move forward, s back
-    // a move left, d move right
-    switch (key) {
-      case 'w':
-      case 'W':
-      case '&':
-        wKey = false;
-        break;
-      case 's':
-      case 'S':
-      case '(':
-        sKey = false;
-        break;
-      case 'a':
-      case 'A':
-      case '%':
-        aKey = false;
-        break;
-      case 'd':
-      case 'D':
-      case "'":
-        dKey = false;
-        break;
-      default:
-        break;
-    }
+  // Mouse Down register for Mouse Movement
+  engine.handleMouseDown = function (event) { 
+    mouseIsDown = true;
+    mouseClicked = true;
+    mouseClickedTimer = 0;
   }
-  // Mouse Click/Down register for Mouse Movement
-  engine.handleMouseDown = function (event) {
+  // Mouse Up register for Mouse Movement
+  engine.handleMouseUp = function (event) {
+    mouseIsDown = false;
+  }
+  whileMouseDown = function() {
     var changeX = 2*((engine.getMouseDownX() - engWidth/2) / (engWidth/2));
-    var changeY = 2*((engine.getMouseDownY() - engHeight/2) / (engHeight/2));
-    goalX = zombieSceneNode.Pos.X + changeY * mouseToDist;
-    goalZ = zombieSceneNode.Pos.Z + changeX * mouseToDist;
-    //console.log("Seeking " + changeX + ", " + changeY);
+    var changeY = -2*((engine.getMouseDownY() - engHeight/2) / (engHeight/2));
+    var changeHyp = Math.sqrt(Math.pow(changeX,2) + Math.pow(changeY,2));
+    var theta = Math.atan(changeY / changeX);
+    var rotatedX = changeHyp * Math.sin(theta + Math.PI*5/4);
+    var rotatedY = changeHyp * Math.cos(theta + Math.PI*5/4);
+    if (changeX > 0) { rotatedX = -1*rotatedX; }
+    if (changeX > 0) { rotatedY = -1*rotatedY; }
+    goalX = zombieSceneNode.Pos.X + rotatedY * mouseToDist;
+    goalZ = zombieSceneNode.Pos.Z + rotatedX * mouseToDist;
+    originalX = zombieSceneNode.Pos.X;
+    originalZ = zombieSceneNode.Pos.Z;
+    console.log(theta);
   }
   // Mouse wheel for zooming
   // see http://plugins.jquery.com/project/mousewheel for more info
@@ -178,12 +177,39 @@ $(function() {
     difZ = (difZ * (changeRate - 1) + newZ) / changeRate;
     camDistRatio = camSetDist / (Math.sqrt(Math.pow(difX, 2) + Math.pow(difZ, 2)));
   }
+  // To move any node from position origin to position destination at walkspeed
+  goFromTo = function(origin, destination) {
+    var newVal = 0;
+    // Handle goalX if it has a new number
+    if (destination != origin) {
+      if (destination > origin + 1) {
+        newVal += walkSpeed;
+      } else if (destination < origin - 1) { 
+        newVal -= walkSpeed;
+      } else {
+        destination = origin;
+      }
+    }
+    return newVal;
+  }
   mainLoop = function() {
-    // Timing stuff to make movement independent of framerate, WIP
-    //var secs = (new Date());
-    //timeDiff = secs - lastTime;
-    //lastTime = secs;
     if(zombieSceneNode) {
+
+      // Check to make sure mouse is held down, not just clicked
+      if (mouseIsDown) {
+        if (mouseClicked) {
+          mouseClickedTimer++;
+          if (mouseClickedTimer > 10) {
+            mouseClicked = false;
+          }
+        }
+        whileMouseDown();
+      }
+      // If mouse held and released, stop immediately
+      if (!mouseIsDown && !mouseClicked) {
+        goalX = zombieSceneNode.Pos.X;
+        goalZ = zombieSceneNode.Pos.Z;
+      }
 
       if (!cameraStarted) {
         camFollow(cam, zombieSceneNode);
@@ -208,39 +234,27 @@ $(function() {
         newZ += 1.0;
         zombieSceneNode.Rot.Y = 0; //out of 360
       }
-      // Handle goalX if it has a new number
-      if (goalX != zombieSceneNode.Pos.X) {
-        if (goalX > zombieSceneNode.Pos.X + 1) {
-          newX += walkSpeed;
-        } else if (goalX < zombieSceneNode.Pos.X - 1) { 
-          newX -= walkSpeed;
-        } else {
-          goalX = zombieSceneNode.Pos.X;
-        }
-      }
-      // Handle goalZ if it has a new number
-      if (goalZ != zombieSceneNode.Pos.Z) {
-        if (goalZ > zombieSceneNode.Pos.Z + 1) {
-          newZ += walkSpeed;
-        } else if (goalZ < zombieSceneNode.Pos.Z - 1) { 
-          newZ -= walkSpeed;
-        } else {
-          goalZ = zombieSceneNode.Pos.Z;
-        }
-      }
+      // Try also to 
+      newX += goFromTo(zombieSceneNode.Pos.X, goalX);
+      newZ += goFromTo(zombieSceneNode.Pos.Z, goalZ);
       // Update position and camera if any changes made
       if(newX != 0.0 || newZ != 0.0) {
-        dirAngle = Math.tan(difZ / difX);
+        if (goalX == zombieSceneNode.Pos.X && goalZ == zombieSceneNode.Pos.Z) {
+          dirAngle = Math.atan(difZ / difX);
+          // Classic X/Z movement system
+          zombieSceneNode.Pos.X += newX;
+          zombieSceneNode.Pos.Z += newZ;
+        } else {
+          dirAngle = Math.atan((goalZ - originalZ) / (goalX - originalX));
+          if (goalX > zombieSceneNode.Pos.X) { dirAngle = Math.PI + dirAngle; }
+          zombieSceneNode.Rot.Y = 270 - dirAngle * 180 / Math.PI; // dirAngle must be converted into 360
+          // Classic X/Z movement system
+          zombieSceneNode.Pos.X -= walkSpeed * Math.sin(Math.PI/2 + dirAngle);
+          zombieSceneNode.Pos.Z += walkSpeed * Math.cos(Math.PI/2 + dirAngle);
+
+        }
         updatePos(zombieSceneNode, newX, newZ);
 
-        // Classic X/Z movement system
-        zombieSceneNode.Pos.X += newX;
-        //moveRate * timeDiff;
-        zombieSceneNode.Pos.Z += newZ;
-        //moveRate * timeDiff;
-        // Experimental alternative movement system, WIP
-        //zombieSceneNode.Pos.X += newX * Math.sin(dirAngle) + newZ * Math.cos(Math.PI / 2 - dirAngle);//moveRate * timeDiff;
-        //zombieSceneNode.Pos.Z += newX * Math.cos(Math.PI / 2 - dirAngle) +  newX * Math.sin(dirAngle);//moveRate * timeDiff;
         // Finally, update Camera for new positions
         camFollow(cam, zombieSceneNode);
       }
@@ -249,8 +263,6 @@ $(function() {
 
     setTimeout('mainLoop();', 20);
   }
-  // Timing stuff to make movement independent of framerate, WIP
-  //var secs = (new Date()).getSeconds();
   mainLoop()
   
 });
