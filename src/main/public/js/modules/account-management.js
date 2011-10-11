4106401f3d4390be7dee5b06ac7617c82c84e468
Forns <forneyandrew@gmail.com>
2011-10-10 19:11:13 -0700

    - Added some security features to the registration and login

--- src/main/public/js/modules/account-management.js
+++ src/main/public/js/modules/account-management.js
@@ -7,7 +7,12 @@
 module.exports = function (app, client) {
     // Imports for the validation
     var check = require('validator').check,
-    sanitize = require('validator').sanitize;
+    sanitize = require('validator').sanitize,
+    
+    // Helper function to sanitize pre-DB layer user input
+    sanitizeAuthentication = function (userInput) {
+        return (userInput !== sanitize(userInput).xss());
+    };
     
     
     /***** LOGIN ROUTERS *****/
@@ -32,30 +37,42 @@ module.exports = function (app, client) {
             user = inputs.user,
             pass = inputs.pass,
             session = req.session;
-            
-        // Perform database check for authentication
-        // TODO: TOTALLY SECURE. NO ONE COULD POSSIBLY ATTACK THIS WHATSOEVER. LIKE FORT KNOX UP IN THIS...
-        client.query(
-            'select accountId, accountName from ' + client.ACCOUNTS_TABLE
-            + ' where accountName="' + user + '" and password="'
-            + pass + '"',
-            function (err, results, fields) {
-                // If there's an error, report it then reload the page
-                if (err) {
-                    console.log(err);
-                    loginFlash = "An error has occurred; try again shortly...";
-                    res.redirect('back');
-                } else if (results[0]) { // Otherwise, see if there was a match
-                    session.userInfo = results[0];
-                    session.is_logged_in = true;
-                    loginFlash = undefined;
-                    res.redirect('/main');
-                } else { // Or, no match
-                    loginFlash = "Username / Password combination not found...";
-                    res.redirect('home');
+        
+        // Sanitize the user input before running through DB
+        if (sanitizeAuthentication(user) || sanitizeAuthentication(pass)) {
+            req.loginBadFlash = "Errors found in username / password; try again...";
+            res.render('index', {
+                layout : true,
+                loginBadFlash : req.loginBadFlash
+            });
+        } else {
+            // Perform database check for authentication
+            // TODO: TOTALLY SECURE. NO ONE COULD POSSIBLY ATTACK THIS WHATSOEVER. LIKE FORT KNOX UP IN THIS...
+            client.query(
+                'select accountId, accountName from ' + client.ACCOUNTS_TABLE
+                + ' where accountName="' + user + '" and password="'
+                + pass + '"',
+                function (err, results, fields) {
+                    // If there's an error, report it then reload the page
+                    if (err) {
+                        console.log(err);
+                        req.loginBadFlash = "An error has occurred; try again shortly...";
+                    } else if (results[0]) { // Otherwise, see if there was a match
+                        session.userInfo = results[0];
+                        session.is_logged_in = true;
+                        res.redirect('/main');
+                    } else { // Or, no match
+                        req.loginBadFlash = "Username / Password combination not found...";
+                    }
+                    
+                    // Return to the index with any error messages
+                    res.render('index', {
+                        layout : true,
+                        loginBadFlash : req.loginBadFlash
+                    });
                 }
-            }
-        );
+            );
+        }
     });
     
     
@@ -93,90 +110,93 @@ module.exports = function (app, client) {
     app.post('/register', function(req, res) {
         // Variables to collect database queries
         var inputs = req.body,
-            email = inputs.email
-            user = inputs.user
-            pass1 = inputs.pass1
-            pass2 = inputs.pass2,
+            email = inputs.email,
+            user = inputs.user,
+            pass1 = inputs.pass1,
+            pass2 = inputs.pass2;
         
-        // Perform database checking for duplicates
-        client.query(
-            'SELECT accountName, email FROM ' + client.ACCOUNTS_TABLE + 
-            ' WHERE email="' + email + '" OR accountName="' + user + '"',
-            function (err, results, fields) {
-                // If there are errors for some reason record them
-                if (err) {
-                    req.registerFlash = "Hey something went wrong, I'd try again...";
-                }
-                
-                // Check that the passwords match
-                if (pass1 !== pass2) {
-                    req.registerPassFlash = "Your passwords don't match... type... very... carefully...";
-                }
-                
-                // Next, the password
-                try {
-                    // Protect against XSS attack
-                    if (pass1 !== sanitize(pass1).xss()) {throw new Error}
-                } catch (e) {
-                    req.registerPassFlash = "Your password contains some suspicious characters, try another...";
-                }
-                
-                try {
-                    check(pass1).len(7);
-                } catch (e) {
-                    req.registerPassFlash = "Your password must be at least 7 characters long...";
-                }
-                
-                // Only perform checks if results were found from the query
-                if (results.length != 0) {
-                    if (results[0].accountName === user) {
-                        req.registerUserFlash = "Looks like the username " + user + " was taken. Be more creative...";
-                    }
-                      
-                    if (results[0].email === email) {
-                        req.registerEmailFlash = "You, or an impostor, have already registered " + email + "...";
-                    }
-                } else { // If there were not duplicates found in the db, check that the strings are valid
-                    // Start with validating and sanitizing the username
-                    try {
-                        check(user).notNull().is("^[a-zA-Z0-9_]*$");
-                    } catch (e) {
-                        req.registerUserFlash = "Your username must contain at least 1 character choosing from numbers and letters alone..."
+        // Sanitize the user input before running through DB
+        if (sanitizeAuthentication(user) || sanitizeAuthentication(pass1)
+         || sanitizeAuthentication(pass2) || sanitizeAuthentication(email)) {
+            req.registerFlash = "Errors found in registration fields; try again...";
+            res.render('register', {
+                layout : true,
+                registerFlash : req.registerFlash
+            });
+        } else {
+            // Perform database checking for duplicates
+            client.query(
+                'SELECT accountName, email FROM ' + client.ACCOUNTS_TABLE + 
+                ' WHERE email="' + email + '" OR accountName="' + user + '"',
+                function (err, results, fields) {
+                    // If there are errors for some reason record them
+                    if (err) {
+                        req.registerFlash = "Hey something went wrong, I'd try again...";
                     }
                     
-                    // Finally, the email
+                    // Check that the passwords match
+                    if (pass1 !== pass2) {
+                        req.registerPassFlash = "Your passwords don't match... type... very... carefully...";
+                    }
+                    
                     try {
-                        check(email).notNull().isEmail();
+                        check(pass1).len(7);
                     } catch (e) {
-                        req.registerEmailFlash = "Your email must be of the form somebody@something.huh and cannot be blank...";
+                        req.registerPassFlash = "Your password must be at least 7 characters long...";
+                    }
+                    
+                    // Only perform checks if results were found from the query
+                    if (results.length != 0) {
+                        if (results[0].accountName === user) {
+                            req.registerUserFlash = "Looks like the username " + user + " was taken. Be more creative...";
+                        }
+                          
+                        if (results[0].email === email) {
+                            req.registerEmailFlash = "You, or an impostor, have already registered " + email + "...";
+                        }
+                    } else { // If there were not duplicates found in the db, check that the strings are valid
+                        // Start with validating and sanitizing the username
+                        try {
+                            check(user).notNull().is("^[a-zA-Z0-9_]*$");
+                        } catch (e) {
+                            req.registerUserFlash = "Your username must contain at least 1 character choosing from numbers and letters alone..."
+                        }
+                        
+                        // Finally, the email
+                        try {
+                            check(email).notNull().isEmail();
+                        } catch (e) {
+                            req.registerEmailFlash = "Your email must be of the form somebody@something.huh and cannot be blank...";
+                        }
+                    }
+                    
+                    // If there are errors, return to the register page with messages
+                    if (req.registerFlash || req.registerUserFlash || req.registerEmailFlash || req.registerPassFlash) {
+                        res.render('register', {
+                            registerFlash : req.registerFlash,
+                            registerUserFlash : req.registerUserFlash,
+                            registerEmailFlash : req.registerEmailFlash,
+                            registerPassFlash : req.registerPassFlash,
+                            layout : true
+                        });
+                    } else { // Otherwise, no errors, return to the login page with a success after adding to DB
+                        // First, add data to DB  
+                        client.query(
+                            'insert into ' + client.ACCOUNTS_TABLE + " "
+                            + "set accountName = '" + user + "', "
+                            + "password = '" + pass1 + "', "
+                            + "email = '" + email + "'"
+                        );
+                        
+                        req.loginFlash = 'Account created successfully for ' + req.body.user + '!';
+                        // Next, return to the login screen for them to head in to K'tah!
+                        res.render('index', {
+                            layout : true,
+                            loginFlash : req.loginFlash
+                        });
                     }
                 }
-                
-                // If there are errors, return to the register page with messages
-                if (req.registerFlash || req.registerUserFlash || req.registerEmailFlash || req.registerPassFlash) {
-                    res.render('register', {
-                        registerFlash : req.registerFlash,
-                        registerUserFlash : req.registerUserFlash,
-                        registerEmailFlash : req.registerEmailFlash,
-                        registerPassFlash : req.registerPassFlash,
-                        layout : true
-                    });
-                } else { // Otherwise, no errors, return to the login page with a success after adding to DB
-                    // First, add data to DB  
-                    client.query(
-                        'insert into ' + client.ACCOUNTS_TABLE + " "
-                        + "set accountName = '" + user + "', "
-                        + "password = '" + pass1 + "', "
-                        + "email = '" + email + "'"
-                    );
-                
-                    // Next, return to the login screen for them to head in to K'tah!
-                    res.render('index', {
-                        layout : true,
-                        loginFlash : 'Account created successfully for ' + req.body.user + '!'
-                    });
-                }
-            }
-        );
+            );
+        }
     });
 }
