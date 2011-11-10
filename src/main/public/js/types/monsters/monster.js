8da217be293cede05309364b5514e66db769f41d
David Hara <hakuhen@gmail.com>
2011-11-10 00:19:47 -0800

    Beginings of monster movement, targets SHOULD be set, now they just need to actually move.

--- src/main/public/js/types/monsters/monster.js
+++ src/main/public/js/types/monsters/monster.js
@@ -11,7 +11,19 @@ $(function () {
       id: null,
       health: 100,
       posX: 0,
-      posZ: 0
+      posZ: 0,
+      sceneNode: null,
+      target: 0
+    },
+    
+    updateTarget: function (players) {
+      var closestDistance = null;
+      for(var i = 0; i < players.length; i++) {
+        if (closestDistance === null || closestDistance > this.sceneNode.Pos.getDistanceTo(new CL3D.Vect3d(players[i].posX, 1.3, players[i].posZ))) {
+          closestDistance = this.sceneNode.Pos.getDistanceTo(new CL3D.Vect3d(players[i].posX, 1.3, players[i].posZ));
+          this.target = i;
+        }
+      }
     }
   });
 });
