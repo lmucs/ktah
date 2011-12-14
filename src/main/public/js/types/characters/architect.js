c222092873461f20690018c7d4f1a5a0f1278597
BlinksTale <BlinksTale@gmail.com>
2011-12-13 19:21:05 -0800

    Switched path ability over to pioneer

--- src/main/public/js/types/characters/architect.js
+++ src/main/public/js/types/characters/architect.js
@@ -46,19 +46,6 @@ $(function () {
         },
         
         function () {
-          var playerPosition = that.sceneNode.Pos,
-              abilityNumber = 1,
-              cooldown = 10;
-          if (!that.cooldowns[abilityNumber]) {
-            ktah.abilities.postAbilityUse("path", playerPosition.X, playerPosition.Y, playerPosition.Z, that.sceneNode.Rot.Y, cooldown);
-            that.cooldowns[abilityNumber] = 0.1;//quick cooldown, almost none! cooldown;
-            that.fadeAbilities(abilityNumber, cooldown);
-            that.tickCooldown(abilityNumber);
-            // Ability point bonus
-            ktah.util.queuedPoints += 1;
-            return -1;
-          }
-          return that.cooldowns[abilityNumber];
         },
         
         function () {
